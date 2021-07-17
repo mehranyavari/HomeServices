509c15121fdf5f43d81468b5c7171a7014f36fc6
mehran yavari <mehranyavari@yahoo.com>
2021-07-17 23:30:20 +0430

    create first wordpres theme

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ *Main template file.
+ *
+ * @package Home Services
+ */
+?>
+<!doctype html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wordpress Theme</title>
+</head>
+<body>
+<h1 style="text-align: center">سلام وردپرس</h1>
+</body>
+</html>
