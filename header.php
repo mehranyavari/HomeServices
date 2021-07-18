7f1e18aa6b0ea9208fd56d67a523ef09d084d0c8
mehran yavari <mehranyavari@yahoo.com>
2021-07-18 23:27:20 +0430

    dynamic lang and charset in header

--- header.php
+++ header.php
@@ -7,13 +7,12 @@
  */
 ?>
 <!doctype html>
-<html lang="fa">
+<html lang="<?php language_attributes(); ?>">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Wordpress Theme</title>
 </head>
 <body>
 <header style="background: palevioletred;text-align: center">My Header</header>
