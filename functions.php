191dafcf2a5f6fcc8c33489c764d4040feaf8da3
mehran yavari <mehranyavari@yahoo.com>
2021-07-18 23:47:00 +0430

    create homei_enqueue_scripts function for set style

--- functions.php
+++ functions.php
@@ -5,3 +5,12 @@
  *
  * @package Home Services
  */
+
+function homei_enqueue_scripts()
+{
+    //wp_enqueue_style('stylesheet',get_template_directory_uri().'/style.css');
+    //wp_enqueue_style('main', get_template_directory().'/main.css', [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+}
+
+add_action('wp_enqueue_scripts', 'homei_enqueue_scripts');
