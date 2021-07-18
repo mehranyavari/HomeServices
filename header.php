f78703f7d8caf6bd8c163d3c487a3caf69a9d9e2
mehran yavari <mehranyavari@yahoo.com>
2021-07-19 01:03:48 +0430

    add if for exists wp_body_open in header

--- header.php
+++ header.php
@@ -16,6 +16,11 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class('hello-class'); ?>>
-<?php wp_body_open(); ?>
+
+<?php
+if (function_exists('wp_body_open')) {
+    wp_body_open();
+}
+?>
 
 <header style="background: palevioletred;text-align: center">My Header</header>
