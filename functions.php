9b5b5db2da6e0e136fe54ffa027a41a337cb6528
mehran yavari <mehranyavari@yahoo.com>
2021-07-19 00:07:36 +0430

    use wp_register_style and wp_register_script instead of directly call i

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ function homei_enqueue_scripts()
 
     #1 register styles and scripts
     wp_register_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
-    wp_register_script('main', get_template_directory_uri() . 'assets/main.js', [], filemtime(get_template_directory() . 'assets/main.js'), true);
+    wp_register_script('main', get_template_directory_uri() . 'assets/main.js', [], filemtime(get_template_directory() . '/assets/main.js'), true);
 
     #2 call styles and scripts
     wp_enqueue_style('stylesheet');
