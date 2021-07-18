1ad5d4402018ea8946986ae13c044eeaffd1082f
mehran yavari <mehranyavari@yahoo.com>
2021-07-19 00:00:15 +0430

    set main.js with wp_enqueue_script in functions

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@ function homei_enqueue_scripts()
     //wp_enqueue_style('stylesheet',get_template_directory_uri().'/style.css');
     //wp_enqueue_style('main', get_template_directory().'/main.css', [], filemtime(get_template_directory() . '/style.css'), 'all');
     wp_enqueue_style('stylesheet', get_stylesheet_uri(), [], filemtime(get_template_directory() . '/style.css'), 'all');
+    wp_enqueue_script('main',get_template_directory_uri().'assets/main.js',[],filemtime(get_template_directory() . 'assets/main.js'),true);
 }
 
 add_action('wp_enqueue_scripts', 'homei_enqueue_scripts');
