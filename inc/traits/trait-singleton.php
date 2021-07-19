e7cfaf77ed613aea31007387996ff3d6dceb22d8
mehran yavari <mehranyavari@yahoo.com>
2021-07-20 02:00:47 +0430

    added trait-singleton in traits

---
+++ inc/traits/trait-singleton.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Homei\Inc\Traits;
+
+trait Singleton
+{
+    public function __construct()
+    {
+    }
+
+    public function __clone()
+    {
+
+    }
+
+    final public static function get_instance()
+    {
+        static $instance = [];
+        $called_class = get_called_class();
+
+        if (!isset($instance[$called_class])) {
+            $instance[$called_class] = new $called_class;
+            do_action(sprintf('homei_sigleton_init%s', $called_class));
+        }
+        return $instance[$called_class];
+    }
+}
