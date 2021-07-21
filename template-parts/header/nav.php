e11dd0978104d3b14bdda2444c543e919ae92953
mehran yavari <mehranyavari@yahoo.com>
2021-07-22 02:56:28 +0430

    show header  menu itmes

--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -4,6 +4,12 @@
  *
  * @package Home Services
  */
+
+use HOMEI_THEME\Inc\Menus;
+
+$menu_class = Menus::get_instance();
+$header_menu_id = $menu_class->get_menu_id('header-menu');
+$header_menus = wp_get_nav_menu_items($header_menu_id);
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
@@ -12,36 +18,44 @@
             the_custom_logo();
         }
         ?>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                       data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
+            <?php if (!empty($header_menus) && is_array($header_menus)): ?>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <?php foreach ($header_menus as $menu_item):
+                        if (!$menu_item->menu_item_parent):
+                            $child_menu_items = $menu_class->get_child_menu_items($header_menus, $menu_item->ID);
+
+                            $has_children = !empty($child_menu_items) && is_array($child_menu_items);
+
+                            if (!$has_children):
+                                ?>
+                                <li class="nav-item">
+                                    <a class="nav-link"
+                                       href="<?= esc_url($menu_item->url); ?>"><?= esc_html($menu_item->title); ?></a>
+                                </li>
+                            <?php else: ?>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="<?= esc_url($menu_item->url); ?>"
+                                       id="navbarDropdown" role="button"
+                                       data-bs-toggle="dropdown" aria-expanded="false">
+                                        <?= esc_html($menu_item->title); ?>
+                                    </a>
+                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <?php foreach ($child_menu_items as $child_menu_item): ?>
+                                            <li><a class="dropdown-item"
+                                                   href="<?= esc_url($child_menu_item->url); ?>"><?= esc_html($child_menu_item->title); ?></a>
+                                            </li>
+                                        <?php endforeach; ?>
+                                    </ul>
+                                </li>
+                            <?php endif; endif; endforeach; ?>
+                </ul>
+            <?php endif; ?>
             <form class="d-flex">
                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Search</button>
@@ -49,11 +63,3 @@
         </div>
     </div>
 </nav>
-<?php
-wp_nav_menu(
-    [
-        'theme_location' => 'header-menu',
-        'container_class' => 'my_extra_menu_class'
-    ]
-);
-?>
