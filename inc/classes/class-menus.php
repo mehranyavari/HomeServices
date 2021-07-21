149d6b7a6c886fc753cac1d6c130d642881d0614
mehran yavari <mehranyavari@yahoo.com>
2021-07-22 02:55:51 +0430

    create get_menu_id and get_child_menu_items funcs for get menu items

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -27,8 +27,32 @@ class Menus
     public function register_menus()
     {
         register_nav_menus([
-            'header-menu' => esc_html__('منوی اصلی','homei'),
-            'footer-menu' => esc_html__('منوی فوتر','homei')
+            'header-menu' => esc_html__('منوی اصلی', 'homei'),
+            'footer-menu' => esc_html__('منوی فوتر', 'homei')
         ]);
+    }
+
+    public function get_menu_id($location)
+    {
+        //Get all locations.
+        $locations = get_nav_menu_locations();
+
+        //Get object id by location.
+        $menu_id = $locations[$location];
+
+        return !empty($menu_id) ? $menu_id : '';
+    }
+
+    public function get_child_menu_items($menu_array, $parent_id)
+    {
+        $child_menus = [];
+        if (!empty($menu_array) && is_array($menu_array)) {
+            foreach ($menu_array as $menu) {
+                if (intval($menu->menu_item_parent) === $parent_id) {
+                    array_push($child_menus, $menu);
+                }
+            }
+        }
+        return $child_menus;
     }
 }
