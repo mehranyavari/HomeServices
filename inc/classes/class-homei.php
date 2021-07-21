d3a475440a32953258b1a97c4a7ee0114202718e
mehran yavari <mehranyavari@yahoo.com>
2021-07-21 19:45:30 +0430

    add custom-background

--- inc/classes/class-homei.php
+++ inc/classes/class-homei.php
@@ -34,12 +34,18 @@ class HOMEI_THEME
     {
         add_theme_support('title-tag');
 
-        add_theme_support('custom-logo',[
-            'header-text'          => ['site-title', 'site-description'],
-            'height'               => 100,
-            'width'                => 400,
-            'flex-height'          => true,
-            'flex-width'           => true,
+        add_theme_support('custom-logo', [
+            'header-text' => ['site-title', 'site-description'],
+            'height' => 100,
+            'width' => 400,
+            'flex-height' => true,
+            'flex-width' => true,
+        ]);
+
+        add_theme_support('custom-background', [
+            'default-color' => '#fff',
+            'default-image' => '',
+            'default-repeat' => 'no-repeat',
         ]);
     }
 }
