efe8cbe0d2288df3fb90f77e3150b4c7182e7871
mehran yavari <mehranyavari@yahoo.com>
2021-07-25 01:25:50 +0430

    add loop posts in index

--- index.php
+++ index.php
@@ -8,8 +8,18 @@
 ?>
 <?php get_header() ?>
 
-    <div class="content">
-        <?php esc_html_e('Content Index', 'homei'); ?>
+    <div id="primary">
+        <main id="main" class="site-main mt-5" role="main">
+            <?php if (have_posts()): ?>
+                <div class="container">
+                    <?php while (have_posts()): the_post();
+                        the_title();
+                        the_content();
+                    endwhile;
+                    ?>
+                </div>
+            <?php endif; ?>
+        </main>
     </div>
 
 <?php get_footer() ?>
