ec908782a3c2d1db955b19b020a82e0ae1f10d47
mehran yavari <mehranyavari@yahoo.com>
2021-07-25 00:23:17 +0430

    create front-page

---
+++ front-page.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Front Page Template.
+ *
+ * @package Home Services
+ */
+?>
+<?php get_header() ?>
+
+    <div class="content">
+        <?php esc_html_e('Front Page', 'homei'); ?>
+    </div>
+
+<?php get_footer() ?>
