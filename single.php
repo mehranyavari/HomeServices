f09c05c6eb057c86f27b6d1a6e827823c1128880
mehran yavari <mehranyavari@yahoo.com>
2021-07-25 00:25:55 +0430

    create single for post

---
+++ single.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Single Post Template.
+ *
+ * @package Home Services
+ */
+?>
+<?php get_header() ?>
+
+    <div class="content">
+        <?php esc_html_e('Single Post', 'homei'); ?>
+    </div>
+
+<?php get_footer() ?>
