30bb8888977472beec69c0de021fd9847968edad
mehran yavari <mehranyavari@yahoo.com>
2021-07-25 00:24:55 +0430

    create page

---
+++ page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Page Template.
+ *
+ * @package Home Services
+ */
+?>
+<?php get_header() ?>
+
+    <div class="content">
+        <?php esc_html_e('Single Page', 'homei'); ?>
+    </div>
+
+<?php get_footer() ?>
