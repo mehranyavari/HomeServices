d50b33689da3ba892b0f2622a636917308117d09
mehran yavari <mehranyavari@yahoo.com>
2021-07-25 23:38:12 +0430

    grid card for blogs

--- index.php
+++ index.php
@@ -12,11 +12,24 @@
         <main id="main" class="site-main mt-5" role="main">
             <?php if (have_posts()): ?>
                 <div class="container">
-                    <?php while (have_posts()): the_post();
-                        the_title();
-                        the_content();
-                    endwhile;
-                    ?>
+                    <?php if (is_home() && !is_front_page()): ?>
+                        <header class="mb-5">
+                            <h1 class="page-title"><?php single_post_title(); ?></h1>
+                        </header>
+                    <?php endif; ?>
+                    <div class="row">
+                        <?php while (have_posts()): the_post(); ?>
+                            <div class="col-lg-4 col-md-6 col-sm-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <h5 class="card-title"><?php the_title(); ?></h5>
+                                        <p class="card-text"><?php the_excerpt(); ?></p>
+                                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endwhile; ?>
+                    </div>
                 </div>
             <?php endif; ?>
         </main>
