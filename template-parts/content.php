5f0f467dd2446a20109f0e4f6f59e19fb86595e4
mehran yavari <mehranyavari@yahoo.com>
2021-07-28 21:49:26 +0430

    separate content of nlog

---
+++ template-parts/content.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  Content Template
+ *
+ */
+?>
+<div class="card">
+    <div class="card-body">
+        <h5 class="card-title"><?php the_title(); ?></h5>
+        <p class="card-text"><?php the_excerpt(); ?></p>
+        <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+</div>
