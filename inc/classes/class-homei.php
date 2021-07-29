26a8558b4c5fff038762ab499869c7cfda392da8
mehran yavari <mehranyavari@yahoo.com>
2021-07-30 00:22:49 +0430

    add meta box for hide page title

--- inc/classes/class-homei.php
+++ inc/classes/class-homei.php
@@ -18,6 +18,7 @@ class HOMEI_THEME
         //load classes.
         Assets::get_instance();
         Menus::get_instance();
+        Meta_Boxes::get_instance();
         $this->setup_hooks();
     }
 
@@ -30,11 +31,27 @@ class HOMEI_THEME
         add_action('after_setup_theme', [$this, 'setup_theme']);
 
     }
-
+    /**
+     * Setup theme.
+     *
+     * @return void
+     */
     public function setup_theme()
     {
+        /**
+         * Let WordPress manage the document title.
+         * By adding theme support, we declare that this theme does not use a
+         * hard-coded <title> tag in the document head, and expect WordPress to
+         * provide it for us.
+         */
         add_theme_support('title-tag');
 
+        /**
+         * Custom logo.
+         *
+         * @see Adding custom logo
+         * @link https://developer.wordpress.org/themes/functionality/custom-logo/#adding-custom-logo-support-to-your-theme
+         */
         add_theme_support('custom-logo', [
             'header-text' => ['site-title', 'site-description'],
             'height' => 100,
@@ -42,16 +59,44 @@ class HOMEI_THEME
             'flex-height' => true,
             'flex-width' => true,
         ]);
-
+        /**
+         * Adds Custom background panel to customizer.
+         *
+         * @see Enable Custom Backgrounds
+         * @link https://developer.wordpress.org/themes/functionality/custom-backgrounds/#enable-custom-backgrounds
+         */
         add_theme_support('custom-background', [
             'default-color' => '#fff',
             'default-image' => '',
             'default-repeat' => 'no-repeat',
         ]);
-
+        /**
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * Adding this will allow you to select the featured image on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
         add_theme_support('post-thumbnails');
+
+        /**
+         * Register image sizes
+         */
+        add_image_size('featured-large',350,233,true);
+        // Add theme support for selective refresh for widgets.
+        /**
+         * WordPress 4.5 includes a new Customizer framework called selective refresh
+         *
+         * Selective refresh is a hybrid preview mechanism that has the performance benefit of not having to refresh the entire preview window.
+         *
+         * @link https://make.wordpress.org/core/2016/03/22/implementing-selective-refresh-support-for-widgets/
+         */
         add_theme_support('customize-selective-refresh-widgets');
         add_theme_support('automatic-feed-links');
+        /**
+         * Switch default core markup for search form, comment form, comment-list, gallery, caption, script and style
+         * to output valid HTML5.
+         */
         add_theme_support('html5', [
             'search-form',
             'comment-form',
--- inc/classes/class-meta-boxes.php
+++ inc/classes/class-meta-boxes.php
@@ -1,1 +1,56 @@
 <?php
+/**
+ * Register meta boxes.
+ *
+ * @package Home Services
+ */
+
+namespace HOMEI_THEME\Inc;
+
+use HOMEI_THEME\Inc\Traits\Singleton;
+
+class Meta_Boxes
+{
+    use Singleton;
+
+    protected function __construct()
+    {
+        //load classes.
+        $this->setup_hooks();
+    }
+
+    protected function setup_hooks()
+    {
+        /**
+         * Actions
+         */
+        add_action('add_meta_boxes', [$this, 'add_custom_meta_box']);
+    }
+
+    public function add_custom_meta_box()
+    {
+        $screens = ['post'];
+        foreach ($screens as $screen) {
+            add_meta_box(
+                'hide-page-title',
+                __('مخفی کردن عنوان صفحه', 'homei'),
+                [$this, 'custom_meta_box_html'],
+                $screen,
+                'side'
+            );
+        }
+    }
+
+    public function custom_meta_box_html($post)
+    {
+        $value = get_post_meta($post->ID, '_hide_page_title', true);
+        ?>
+        <label for="homei-field"><?php esc_html_e('عنوان صفحه را مخفی کنید', 'homei'); ?></label>
+        <select name="homei_field" id="homei-field" class="postbox" style="margin-bottom: 0 !important;">
+            <option value=""><?php esc_html_e('یکی را انتخاب کنید', 'homei'); ?></option>
+            <option value="yes" <?php selected($value, 'yes'); ?>><?php esc_html_e('بله', 'homei'); ?></option>
+            <option value="no" <?php selected($value, 'no'); ?>><?php esc_html_e('خیر', 'homei'); ?></option>
+        </select>
+        <?php
+    }
+}
