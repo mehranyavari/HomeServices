4a0fbf1694cc0864ab3ce1ad16bcb8801b2e01ef
mehran yavari <mehranyavari@yahoo.com>
2021-07-29 17:37:52 +0430

    include template-tags

--- functions.php
+++ functions.php
@@ -10,20 +10,16 @@ if (!defined('HOMEI_DIR_PATH')) {
     define('HOMEI_DIR_PATH', untrailingslashit(get_template_directory()));
 }
 
-if (!defined('HOMEI_DIR_URI')){
+if (!defined('HOMEI_DIR_URI')) {
     define('HOMEI_DIR_URI', untrailingslashit(get_template_directory_uri()));
 }
 require_once HOMEI_DIR_PATH . '/inc/helpers/autoloader.php';
-require_once HOMEI_DIR_PATH.'/inc/classes/class-homei.php';
+require_once HOMEI_DIR_PATH . '/inc/classes/class-homei.php';
+require_once HOMEI_DIR_PATH . '/inc/helpers/template-tags.php';
 
-function homei_get_theme_instance(){
+function homei_get_theme_instance()
+{
     \HOMEI_THEME\Inc\HOMEI_THEME::get_instance();
 }
+
 homei_get_theme_instance();
-
-function homei_enqueue_scripts()
-{
-
-}
-
-add_action('wp_enqueue_scripts', 'homei_enqueue_scripts');
