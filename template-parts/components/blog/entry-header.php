bad92e8deb07e490f2adafaf85aabff0eee22f27
mehran yavari <mehranyavari@yahoo.com>
2021-07-29 17:37:15 +0430

    create entry header

--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -1,1 +1,27 @@
 <?php
+/**
+ * Template for post entry header
+ *
+ * @package Home Services
+ */
+$the_post_id = get_the_ID();
+$has_post_thumbnail = get_the_post_thumbnail($the_post_id);
+?>
+
+<header class="entry-header">
+    <!--    Featured image-->
+    <?php if ($has_post_thumbnail): ?>
+        <div class="entry-image mb-3">
+            <a href="<?= esc_url(get_permalink()) ?>">
+                <?php the_post_custom_thumbnail(
+                    $the_post_id,
+                    'featured-large',
+                    [
+                        'sizes' => '(max-width:590px) 590px,425px',
+                        'class' => 'attachment-featured-large size-featured-image'
+                    ]
+                ); ?>
+            </a>
+        </div>
+    <?php endif; ?>
+</header>
