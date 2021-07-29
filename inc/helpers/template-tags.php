5fe9d3f214a039338279db88c88624dcb4315302
mehran yavari <mehranyavari@yahoo.com>
2021-07-29 17:35:46 +0430

    create template tags

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -1,1 +1,35 @@
 <?php
+
+function get_the_post_custom_thumbnail($post_id, $size = 'featured-large', $additional_attributes = []): string
+{
+    $custom_thumbnail = '';
+
+    if (null === $post_id) {
+        $post_id = get_the_ID();
+    }
+
+    if (has_post_thumbnail($post_id)) {
+        $default_attributes = [
+            'loading' => 'lazy',
+        ];
+        $attrs = array_merge($additional_attributes, $default_attributes);
+
+        $custom_thumbnail = wp_get_attachment_image(
+            get_post_thumbnail_id($post_id),
+            $size,
+            false,
+            $additional_attributes
+        );
+    }
+    return $custom_thumbnail;
+}
+
+/**
+ * @param $post_id
+ * @param string $size
+ * @param array $additional_attributes
+ */
+function the_post_custom_thumbnail($post_id, $size = 'featured-large', $additional_attributes = [])
+{
+    echo get_the_post_custom_thumbnail($post_id, $size, $additional_attributes);
+}
